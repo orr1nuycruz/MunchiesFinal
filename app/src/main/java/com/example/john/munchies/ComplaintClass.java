84a0d594919b3340b87b3c8249c121318f15a286
Kapilan <Kapster03@gmail.com>
2019-03-20 20:10:20 -0400

    Added complaint layout

---
+++ app/src/main/java/com/example/john/munchies/ComplaintClass.java
@@ -0,0 +1,46 @@
+package com.example.john.munchies;
+
+public class ComplaintClass {
+    String Name, Phone, OrderId, Issue;
+
+    public void ComplaintClass(String id, String name, String phone, String issue)
+    {
+        id = OrderId;
+        name = Name;
+        phone = Phone;
+        issue = Issue;
+
+    }
+
+    public String getOrderId() {
+        return OrderId;
+    }
+
+    public void setOrderId(String orderId) {
+        OrderId = orderId;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public String getIssue() {
+        return Issue;
+    }
+
+    public void setIssue(String issue) {
+        Issue = issue;
+    }
+}
