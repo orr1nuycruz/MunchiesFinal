35fe4a4be32d01438619b0888756fc7783b5203e
Harvey <crazyapple077@yahoo.com>
2019-04-04 15:33:52 -0400

    Include conditions for AdminAddRestaurant

--- app/src/main/java/com/example/john/munchies/AdminAddRestaurant.java
+++ app/src/main/java/com/example/john/munchies/AdminAddRestaurant.java
@@ -46,6 +46,7 @@ public class AdminAddRestaurant extends AppCompatActivity {
         editRestaurantID = (EditText)findViewById(R.id.editAdminAddResID);
         btnAddRestaurant = (Button)findViewById(R.id.AddRestaurant);
         btnDeleteRestaurant = (Button)findViewById(R.id.DeleteRestaurant);
+        btnDeleteRestaurant.setEnabled(false);
         addRestaurant();
 
         restaurantList = (ListView)findViewById(R.id.restaurantListView);
@@ -81,7 +82,7 @@ public class AdminAddRestaurant extends AppCompatActivity {
 
 
         restaurantAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, restaurantArrayList);
+                android.R.layout.simple_list_item_single_choice, restaurantArrayList);
         restaurantList.setAdapter(restaurantAdapter);
 
     }
@@ -111,12 +112,35 @@ public class AdminAddRestaurant extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 restaurant.setRestaurantID(restaurantArrayList.get(position));
+                editRestaurantName.setText(restaurantArrayList.get(position));
+
+                //Enable or Disable Buttons
+                btnDeleteRestaurant.setEnabled(true);
+                btnAddRestaurant.setEnabled(false);
+
+                //Disable editing
+                editRestaurantName.setEnabled(false);
+                editRestaurantID.setEnabled(false);
+
+
             }
         });
 
         btnDeleteRestaurant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //Enable or Disable Buttons
+                btnDeleteRestaurant.setEnabled(false);
+                btnAddRestaurant.setEnabled(true);
+
+                //Enable Editing
+                editRestaurantName.setEnabled(true);
+                editRestaurantID.setEnabled(true);
+
+                //Clear Editing
+                editRestaurantName.setText("");
+                editRestaurantID.setText("");
                 final String name = restaurant.getRestaurantID();
                 Toast.makeText(getApplicationContext(), name, Toast.LENGTH_SHORT).show();
                 if(name.equals("")){
